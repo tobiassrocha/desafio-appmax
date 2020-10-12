2f035707642fa648c071f98ed30f4e67197c2a01
tobias.rosa <tobias.rosa@CTNAPPLE19KLVDD.local>
2020-10-12 02:08:23 -0300

    Header component - css done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,80 @@
 <template>
-    <header id="nav">
-      <a href="/">Home</a>
-      <a href="/create-form-page">Criar nova oferta</a>
+    <header class="header">
+      <div class="header__container">
+        <a href="/">
+          <h1 class="header__logo"></h1>
+        </a>
+        <nav class="navbar">
+          <ul class="navbar__list">
+            <li class="navbar__item">
+              <a href="/" class="navbar__link">Home</a>
+            </li>
+            <li class="navbar__item">
+              <a href="/create-form-page" class="navbar__link">Criar nova oferta</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
 </template>
 
-<style>
+<style lang="less" escoped>
+.header {
+  height: 95px;
+  margin-bottom: 30px;
+  
+  &__container {
+    background-color: #fff;
+    border-bottom: 1px solid #000;
+    box-sizing: border-box;
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 30px;
+    position: fixed;
+    width: 100%;
+  }
+  &__logo {
+    background-image: url('../assets/logo.png');
+    background-size: 100%;
+    background-repeat: no-repeat;
+    margin: 0;
+    width: 300px;
+    height: 75px;
+  }
 
+  .navbar {
+    display: flex;
+
+    &__list {
+      display: flex;
+      list-style: none;
+      margin: 0;
+      padding: 0;
+      align-items: center;
+    }
+    
+    &__item {
+      font-size: 16px;
+      text-decoration: none;
+
+      &:first-child {
+        margin-right: 15px;
+      }
+    }
+    
+    &__link {
+      color: #000;
+      font-size: 16px;
+      font-weight: bold;
+      text-decoration: none;
+      border-radius: 5px;
+      border: 1px solid;
+      padding: 5px 15px;
+
+      &:hover {
+        background-color: #fcbf10;
+      }
+    }
+  }
+}
 </style>
